a461a6a0e799b96201fd0d136c15100b871e8857
ijklop90 <72297730+ijklop90@users.noreply.github.com>
2020-10-03 12:39:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<script src="http://code.jquery.com/jquery.min.js"></script>
+		<script src="js/jquery-ui.min.js"></script>
+		<script src="js/main.js"></script>
+	</head>
+	<body>
+		<div id="datepicker">
+			
+		</div>
+		<div id="dialog" title="Basic dialog">
+  </div>
+<input type="hidden" id="datepicker_value" value="01.08.2019">
+	</body>
+</html>
